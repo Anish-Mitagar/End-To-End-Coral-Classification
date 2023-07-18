f7ec79478424178bc7f1cca02f8116875e309434
anishmitagar <Anish.S.Mitagar@gmail.com>
2023-07-18 15:30:54 -0400

    twelveth deployment attempt

--- app.py
+++ app.py
@@ -12,35 +12,35 @@ app = Flask(__name__)
 #CORS(app)
 
 
-class ClientApp:
-    def __init__(self):
-        self.filename = "inputImage.jpg"
-        self.classifier = PredictionPipeline(self.filename)
+# class ClientApp:
+#     def __init__(self):
+#         self.filename = "inputImage.jpg"
+#         self.classifier = PredictionPipeline(self.filename)
 
 
 @app.route("/", methods=['GET'])
 #@cross_origin()
 def home():
-    return render_template('index.html')
+    return render_template('index2.html')
 
 
-@app.route("/train", methods=['GET','POST'])
-#@cross_origin()
-def trainRoute():
-    os.system("python3 main.py")
-    return "Training done successfully!"
+# @app.route("/train", methods=['GET','POST'])
+# #@cross_origin()
+# def trainRoute():
+#     os.system("python3 main.py")
+#     return "Training done successfully!"
 
 
 
-@app.route("/predict", methods=['POST'])
-#@cross_origin()
-def predictRoute():
-    image = request.json['image']
-    decodeImage(image, clApp.filename)
-    result = clApp.classifier.predict()
-    return jsonify(result)
+# @app.route("/predict", methods=['POST'])
+# #@cross_origin()
+# def predictRoute():
+#     image = request.json['image']
+#     decodeImage(image, clApp.filename)
+#     result = clApp.classifier.predict()
+#     return jsonify(result)
 
 
 if __name__ == "__main__":
-    clApp = ClientApp()
+    #clApp = ClientApp()
     app.run(host='0.0.0.0', port=8080) #local host
